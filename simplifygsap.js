c30d2c7764ed85255c6d9b06a6111f2606daf76e
lolaraghvendra <64127554+lolaraghvendra@users.noreply.github.com>
2022-07-20 16:03:51 +0530

    added support for default value in stagger-animation

--- simplifygsap.js
+++ simplifygsap.js
@@ -1,32 +1,51 @@
-animated_ele=document.getElementsByClassName("ease-animation");
-
+animated_ele = document.getElementsByClassName("ease-animation");
 
 for (var i = 0; i < animated_ele.length; i++) {
-    element=animated_ele.item(i);
-    var ease =   (undefined ? delete element.duration :( element.dataset.ease));
-    var duration = parseInt  (undefined ? delete element.duration : (element.dataset.duration));
-    var repeat = parseInt   (undefined ? delete element.repeat : ( element.dataset.repeat));
-    var delay = parseInt   (undefined ? delete element.delay: ( element.dataset.delay));
-   
-    gsap.from(element, {duration: duration, x: '-100%',ease:`${ease}`  ,repeat: repeat});
-    
+  element = animated_ele.item(i);
+  var ease = undefined ? delete element.duration : element.dataset.ease;
+  var duration = parseInt(
+    undefined ? delete element.duration : element.dataset.duration
+  );
+  var repeat = parseInt(
+    undefined ? delete element.repeat : element.dataset.repeat
+  );
+  var delay = parseInt(
+    undefined ? delete element.delay : element.dataset.delay
+  );
+
+  gsap.from(element, {
+    duration: duration,
+    x: "-100%",
+    ease: `${ease}`,
+    repeat: repeat,
+  });
 }
-
-animated_ele=document.getElementsByClassName("stagger-animation");
+console.log(ease);
+animated_ele = document.getElementsByClassName("stagger-animation");
 
 for (var i = 0; i < animated_ele.length; i++) {
-    element=animated_ele.item(i);
-   
-    var repeat = parseInt (element.dataset.repeat === undefined ? 2:element.dataset.repeat);
-    var each = parseFloat(undefined ? delete element.each:(element.dataset.each));
-    var ease = undefined ? delete element.ease:(element.dataset.ease);
-    var from = undefined ? delete element.from:(element.dataset.from);
-    var y =parseInt(undefined ? delete element.y:(element.dataset.y));
-    var x =parseInt(undefined ? delete element.x:(element.dataset.x));
-    var yoyo=undefined ? delete element.yoyo:(element.dataset.yoyo);
-    
+    element = animated_ele.item(i);
+    console.log(element.dataset.repeat === undefined);
+    var repeat = parseInt(
+        element.dataset.repeat === undefined ? 2 : element.dataset.repeat
+    );
+    var each = parseFloat(element.dataset.each === undefined ? 0.1 : element.dataset.each);
+    var ease = element.dataset.ease === undefined ? "power1" : element.dataset.ease;
+    var from = element.dataset.from === undefined ? "centre" : element.dataset.from;
+    var y = parseInt(element.dataset.y === undefined ? 100 : element.dataset.y);
+    // var x = parseInt(element.dataset.x === undefined ? delete element.x : element.dataset.x);
+    var yoyo = element.dataset.yoyo === undefined ? true : element.dataset.yoyo;
+
     // console.log(ease);
     // console.log(duration);
-    gsap.from(element,{ease:`${ease}`,stagger:{each:each,from:`${from}`},y:y,repeat:repeat,yoyo:yoyo,x:x});
-    
+    options = {
+        ease: `${ease}`,
+        stagger: { each: each, from: `${from}` },
+        y: y,
+        repeat: repeat,
+        yoyo: yoyo,
+        // x: x,
+    };
+    console.log(options);
+    gsap.from(element, options);
 }
