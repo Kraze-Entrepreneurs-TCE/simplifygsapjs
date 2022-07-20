8874344c4c97a6cd04a779fb9d5b7b915574a02a
Mehreen Sayed <mehreen.syed@thecrazyentrepreneur.com>
2022-07-20 12:04:31 +0530

    Added logic for Stagger

--- simpliyfygsap.js
+++ simpliyfygsap.js
@@ -11,3 +11,23 @@ for (var i = 0; i < animated_ele.length; i++) {
     gsap.from(element, {duration: duration, x: '-100%',ease:`${ease}`  ,repeat: repeat});
     
 }
+
+animated_ele=document.getElementsByClassName("stagger-Animation");
+// console.log(animated_ele);
+
+for (var i = 0; i < animated_ele.length; i++) {
+    element=animated_ele.item(i);
+   
+    var repeat = parseInt (undefined ? delete element.repeat:(element.dataset.repeat));
+    var each = parseFloat(undefined ? delete element.each:(element.dataset.each));
+    var ease = undefined ? delete element.ease:(element.dataset.ease);
+    var from = undefined ? delete element.from:(element.dataset.from);
+    var y =parseInt(undefined ? delete element.y:(element.dataset.y));
+    var x =parseInt(undefined ? delete element.x:(element.dataset.x));
+    var yoyo=undefined ? delete element.yoyo:(element.dataset.yoyo);
+    
+    // console.log(ease);
+    // console.log(duration);
+    gsap.from(element,{ease:`${ease}`,stagger:{each:each,from:`${from}`},y:y,repeat:repeat});
+    
+}
